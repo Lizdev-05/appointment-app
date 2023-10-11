a30f4674e68c072fce4cb77c4aefb2d16edd01ba
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-10-11 06:21:16 +0000

    Create appointment with one way data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
 export class AppointmentListComponent {
-
+  appointments: string = 'O ti ye mi oooo';
 }
